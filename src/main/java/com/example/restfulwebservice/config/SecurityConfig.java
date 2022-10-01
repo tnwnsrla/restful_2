5b0b8c52ae986aa451c515a5d8fe6980f8b6bdec
Jun_Water <tnwnsrla@naver.com>
2022-10-01 20:44:05 +0900

    JPA Controller, Repository 생성

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,10 +3,23 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * H2 Console 접속을 위한 Spring Security 수정
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
